ed31a111e0eb1953c028c72f98018721a7c74e63
Muhammad Khuzaima Umair <75787251+mkhuzaima@users.noreply.github.com>
2022-11-06 22:06:02 +0000

    Add files via upload

---
+++ background.js
@@ -0,0 +1,34 @@
+
+
+// block extension
+listOfBlockedUrls = [
+	"*://partner.googleadservices.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+    "*://*.doubleclick.net/*",
+    "*://*.googlesyndication.com/*",
+    "*://*.googletagservices.com/*",
+    "*://*.google-analytics.com/*",
+    "*://*.googleadservices.com/*",
+    "*://*.zedo.com/*",
+    "*://*.adbrite.com/*",
+    "*://*.adbureau.net/*",
+    "*://*.carbonads.net/*",
+    "*://*.cdn.carbonads.com/*",
+    "*://*.cdn.carbonads.net/*",
+    "*://*.cdn.doubleclick.net/*",
+    "*://*.cdn.googletagservices.com/*",
+    "*://*.cdn.googleadservices.com/*",
+    "*://*.cdn.zedo.com/*"
+]
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function (details) {
+        console.log("Blocking: " + details.url);
+        return { cancel: true };
+    },
+    { urls: listOfBlockedUrls },
+    ["blocking"]
+);
