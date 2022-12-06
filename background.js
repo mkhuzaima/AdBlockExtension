e6dc869cbd2775857c9b47aa16dfd66baf8cb23d
Muhammad Khuzaima Umair <mkhuzaimaumair@gmail.com>
2022-12-06 08:16:17 +0500

    Updated the list of adblock servers

--- background.js
+++ background.js
@@ -1,35 +1,10 @@
 
 
-// block extension
-listOfBlockedUrls = [
-    "*://partner.googleadservices.com/*",
-    "*://creative.ak.fbcdn.net/*",
-    "*://*.exponential.com/*",
-    "*://*.quantserve.com/*",
-    "*://*.scorecardresearch.com/*",
-    "*://*.doubleclick.net/*",
-    "*://*.googlesyndication.com/*",
-    "*://*.googletagservices.com/*",
-    "*://*.google-analytics.com/*",
-    "*://*.googleadservices.com/*",
-    "*://*.zedo.com/*",
-    "*://*.adbrite.com/*",
-    "*://*.adbureau.net/*",
-    "*://*.carbonads.net/*",
-    "*://*.cdn.carbonads.com/*",
-    "*://*.cdn.carbonads.net/*",
-    "*://*.cdn.doubleclick.net/*",
-    "*://*.cdn.googletagservices.com/*",
-    "*://*.cdn.googleadservices.com/*",
-    "*://*.cdn.zedo.com/*",
-    "https://www.youtube.com/get_midroll_info*"
-]
-
 chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
         console.log("Blocking: " + details.url);
         return { cancel: true };
     },
-    { urls: listOfBlockedUrls },
+    { urls: listOfBlockedUrls_v2 },
     ["blocking"]
 );
